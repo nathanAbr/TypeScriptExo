0988f387a020ffa9b7bfd4f064588ec44b8f9802
nathanABR <nathan.abraham@outlook.fr>
2017-08-28 13:50:36 +0200

    exercice 3 .js

---
+++ Exercice 3/exercice3.js
@@ -0,0 +1,52 @@
+/*
+Partie 1
+Créer une classe Personne avec deux attributs : nom et prenom
+et une methode permettant à la personne de se présenter au travers d'un
+message dans la console.
+
+Vérifier la bonne compilation de vote code, inspecter le code produit et
+soumettre le résultat sur votre dépot github
+*/
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Personne = /** @class */ (function () {
+    function Personne() {
+    }
+    Personne.prototype.saluer = function () {
+        return "Bonjour, je m'appelle " + this.nom + " " + this.prenom;
+    };
+    return Personne;
+}());
+/*
+Partie 2
+Créer une classe étudiant, héritant de la classe personne
+Ajout un attribut cursus sur l'étudiant prenant dans un énumération
+de votre création.
+
+Vérifier la bonne compilation de vote code, inspecter le code produit et
+soumettre le résultat sur votre dépot github
+
+*/
+var Cursus;
+(function (Cursus) {
+    Cursus["Informatique"] = "Informatique";
+    Cursus["Langue"] = "Langue";
+    Cursus["Mathematiques"] = "Mathematiques";
+})(Cursus || (Cursus = {}));
+var Etudiant = /** @class */ (function (_super) {
+    __extends(Etudiant, _super);
+    function Etudiant() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.cursus = Cursus.Informatique;
+        return _this;
+    }
+    return Etudiant;
+}(Personne));
