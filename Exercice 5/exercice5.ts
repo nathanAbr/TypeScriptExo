476f84e8e288cf4e3d7c9addd94463ec49850d77
nathanABR <nathan.abraham@outlook.fr>
2017-08-28 14:04:01 +0200

    Exercice 5

--- Exercice 5/exercice5.ts
+++ Exercice 5/exercice5.ts
@@ -15,10 +15,10 @@ Indiquer la hiérachie de type pour chacune des classes suivantes
 sous la forme d'un commentaire au dessus de la définition
 */
 
-/* */
+/* L'étudiant hérite de la classe personne mais n'implémente pas l'interface contact */
 class Etudiant extends Personne {}
 
-/* */
+/* Le professeur implémente l'interface contact qui étant personne*/
 class Professeur {
   private nom: string;
   private prenom: string;
@@ -27,5 +27,5 @@ class Professeur {
 
 /* La déclaration suivante n'est pas valide.
 Expliquez dans un commentaire.
-*/
+La class n'implemente pas l'attribut telephone de l'interface contact et n'implémente pas les attributs nom et prenom étendu de personne par contact*/
 class Stagiaire implements Contact {}
